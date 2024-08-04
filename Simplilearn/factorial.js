2f861bf4b14a33734979ea3a7e92da146edf0a57
Labanya Saha <100686917+Labanya23@users.noreply.github.com>
2024-08-04 11:03:53 +0600

    factorial do while loop

---
+++ Simplilearn/factorial.js
@@ -0,0 +1,14 @@
+//factorial of a number using do-while loop
+
+//5*4*3*2*1
+
+var i=1;
+var n=5;
+var f=1;
+do{
+    f=f*i;
+    i++;
+}
+while(i<=n){
+    document.write("The factorial of \n" +n+"\nis\n"+"<h1>"+f+"</h1");
+}
